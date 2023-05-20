5863b9f6f8fc05dfc33fbb05e0458366011a653b
jobaer-hassan <jobaerhassan3@gmail.com>
2023-05-20 17:06:32 +0600

    selectListener added

--- app/src/main/java/com/example/news_pro/SelectListener.java
+++ app/src/main/java/com/example/news_pro/SelectListener.java
@@ -1,6 +1,9 @@
 package com.example.news_pro;
 
+import com.example.news_pro.Models.NewsHeadlines;
+
 public interface SelectListener {
 
-    
+     void onNewsClicked(NewsHeadlines headlines);
+
 }
