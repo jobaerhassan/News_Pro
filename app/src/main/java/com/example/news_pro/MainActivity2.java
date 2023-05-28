379c5577cf980c3a5b336c976a742c919a338167
jobaer-hassan <jobaerhassan3@gmail.com>
2023-05-28 20:10:36 +0600

    splash screen added

---
+++ app/src/main/java/com/example/news_pro/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.news_pro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);//splash activity
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent  = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+    }
+}
